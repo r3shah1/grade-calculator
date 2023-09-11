8714cfd58cc35492fb1087188f1b3894b6643a69
unknown <r3shah1@umd.edu>
2023-09-11 17:30:12 -0400

    Changes made

--- app/run.py
+++ app/run.py
@@ -12,7 +12,8 @@ weights = GradeWeights()
 
 # Set grades achieved so far
 my_grades.quiz_1 = 0.78 # Received 78% in the first quiz
-
+my_grades.quiz_2 = 0.9 # Received 90% in the second quiz
+my_grades.quiz_3 = 0.95 # Received 95% in the second quiz
 # Print out the grades to console
 print(my_grades)
 
